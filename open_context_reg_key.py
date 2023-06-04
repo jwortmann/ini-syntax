590b32ec70a5f11441897bbbc8eaf1c22d8abf01
Janos Wortmann <jwortmann@outlook.com>
2023-06-04 16:41:18 +0200

    Minor tweaks in plugin file
    No functional change, only style changes and simplification.

--- open_context_reg_key.py
+++ open_context_reg_key.py
@@ -3,10 +3,13 @@ import sublime_plugin
 import subprocess
 
 
+IS_WINDOWS = sublime.platform() == 'windows'
+
+
 class OpenContextRegKeyCommand(sublime_plugin.TextCommand):
     def run(self, edit, event):
-        pt = self.view.window_to_text((event["x"], event["y"]))
-        regions = self.view.find_by_selector("entity.name.section")
+        pt = self.view.window_to_text((event['x'], event['y']))
+        regions = self.view.find_by_selector('entity.name.section')
         for region in regions:
             if region.contains(pt):
                 startupinfo = subprocess.STARTUPINFO()
@@ -14,14 +17,14 @@ class OpenContextRegKeyCommand(sublime_plugin.TextCommand):
                 startupinfo.wShowWindow = 11  # force minimized window
 
                 reg_key = self.view.substr(region)
-                subprocess.call("cmd /c REG ADD HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Applets\\Regedit /v LastKey /t REG_SZ /d \"{}\" /f && start regedit".format(reg_key), startupinfo=startupinfo)
+                subprocess.call('cmd /c REG ADD HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Applets\\Regedit /v LastKey /t REG_SZ /d "{}" /f && start regedit'.format(reg_key), startupinfo=startupinfo)
                 break
 
     def is_enabled(self, event):
-        return sublime.platform() == "windows"
+        return IS_WINDOWS
 
     def is_visible(self, event):
-        return self.is_enabled(event) and self.view.match_selector(self.view.window_to_text((event["x"], event["y"])), "source.reg entity.name.section")
+        return self.is_enabled(event) and self.view.match_selector(self.view.window_to_text((event['x'], event['y'])), 'source.reg entity.name.section')
 
     def want_event(self):
         return True
